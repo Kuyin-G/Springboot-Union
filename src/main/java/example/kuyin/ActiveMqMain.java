6bfcde2fb880bd301c1e7c26cf7aa845d2b4a18a
kuyin-G <kuyin457@163.com>
2020-06-30 22:30:40 +0800

    Springboot项目整合ActiveMQ，新增定时投放功能

--- src/main/java/example/kuyin/ActiveMqMain.java
+++ src/main/java/example/kuyin/ActiveMqMain.java
@@ -4,8 +4,10 @@ package example.kuyin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ActiveMqMain {
 
     public static void main(String[] args) {
--- src/main/java/example/kuyin/Confige/QueueProducer.java
+++ src/main/java/example/kuyin/Confige/QueueProducer.java
@@ -4,10 +4,11 @@ package example.kuyin.Confige;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Queue;
-
+import java.util.UUID;
 
 
 @Component
@@ -22,9 +23,14 @@ public class QueueProducer {
 
     public  void produceMessage(String message){
         // 发送的信息
-//
-
         jmsMessagingTemplate.convertAndSend(queue,message);
+    }
+    // 定时功能：每隔5秒就发送一条信息
+    @Scheduled(fixedDelay = 5000)
+    public void produceMsgScheduled(){
+        String scheduledMsg = "Scheduled Message:"+ UUID.randomUUID().toString().substring(0,6);
+        jmsMessagingTemplate.convertAndSend(queue, scheduledMsg);
+        System.out.println("正在定时投放");
     }
 
 }
